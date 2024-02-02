c140591b3da3fb7abee4a397d633aff31adc1ed5
IbrahimHushki <hushki.ibrahim@gmail.com>
2024-02-02 08:48:32 +0300

    fix build error in script.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,5 @@
 import {
   sampleRUM,
-  buildBlock,
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -19,21 +18,21 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  const h3 = main.querySelector('h3');
-  const buttons = main.querySelector('a');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    for (let i = 0; i < 2; i++) {
-      section.appendChild(buttons[i]);
-    }
-    section.append(buildBlock('hero', { elems: [picture, h1, h3] }));
-    main.prepend(section);
-  }
-}
+// function buildHeroBlock(main) {
+//   const h1 = main.querySelector('h1');
+//   const picture = main.querySelector('picture');
+//   const h3 = main.querySelector('h3');
+//   const buttons = main.querySelector('a');
+//   // eslint-disable-next-line no-bitwise
+//   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+//     const section = document.createElement('div');
+//     for (let i = 0; i < 2; i++) {
+//       section.appendChild(buttons[i]);
+//     }
+//     section.append(buildBlock('hero', { elems: [picture, h1, h3] }));
+//     main.prepend(section);
+//   }
+// }
 
 /**
  * load fonts.css and set a session storage flag
