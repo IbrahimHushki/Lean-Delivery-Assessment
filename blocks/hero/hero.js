07dcf0e56fa39955832390b99c4e0a6989a29b71
IbrahimHushki <hushki.ibrahim@gmail.com>
2024-02-19 03:50:39 +0300

    update selectors and attributes

---
+++ blocks/hero/hero.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+  
+  const heroBody = block.firstElementChild.firstElementChild;
+  const orderBtn = block.querySelector('p:nth-of-type(2)');
+
+  heroBody.classList.add('hero-body');
+  orderBtn.setAttribute('id', 'order-button');
+}
--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,10 +2,10 @@ export default function decorate(block) {
   const quote = block.firstElementChild.firstElementChild;
   const attribution = block.lastElementChild;
 
-  quote.classList.add("quote-body");
-  attribution.classList.add("attribution");
+  quote.classList.add('quote-body');
+  attribution.classList.add('attribution');
 
   quote.textContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-  attribution.textContent = "Cicero";
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  attribution.textContent = 'Cicero';
 }
