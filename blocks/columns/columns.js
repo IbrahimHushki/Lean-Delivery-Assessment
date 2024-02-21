d9b9b5effda466ce9ebbeeda677bea1990799090
Marton Fekete <mafekete89@gmail.com>
2024-02-21 07:19:49 +0100

    Revert "To do list (#3)"
    This reverts commit ff80c8e9a9516ef9ce10bb937e952a33ba7b875f.

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,16 +1,4 @@
 export default function decorate(block) {
-  // Selectors for buttons
-  const firstBtn = document.querySelectorAll('.columns .button-container:nth-of-type(2)');
-  const secondBtn = document.querySelectorAll('.columns .button-container:nth-of-type(3)');
-
-  // Add class to each to button container
-  firstBtn.forEach((btn) => {
-    btn.classList.add('first__button');
-  });
-  secondBtn.forEach((btn) => {
-    btn.classList.add('second__button');
-  });
-
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
--- blocks/hero/hero.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  const heroBody = block.firstElementChild.firstElementChild;
-  const orderBtn = block.querySelector('p:nth-of-type(2)');
-
-  heroBody.classList.add('hero__body');
-  orderBtn.classList.add('order__button');
-}
