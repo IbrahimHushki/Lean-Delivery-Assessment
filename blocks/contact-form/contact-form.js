e6e5a28df31e287c51b3f7d6d7e5bfa8d9f3dd86
IbrahimHushki <hushki.ibrahim@gmail.com>
2024-03-03 18:08:35 +0300

    update: add event listener to the submit contact info button

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,13 +1,18 @@
+import submitContactInfo from './submit-contact-info.js';
+
 // Selectors
 const blockChildDivs = document.querySelectorAll('.contact-form > div');
 
 export default function decorate(block) {
   // Create submit button
-  const submitBtn = document.createElement('button');
-  submitBtn.textContent = 'Submit';
-  submitBtn.classList.add('contact-form__button');
+  const submitBtnDiv = document.createElement('div');
+  const submitBtnLink = document.createElement('a');
+  submitBtnLink.textContent = 'Submit';
+  submitBtnDiv.classList.add('button-container');
+  submitBtnLink.classList.add('contact-form__button');
   // Add submit button
-  block.appendChild(submitBtn);
+  submitBtnDiv.appendChild(submitBtnLink);
+  block.appendChild(submitBtnDiv);
 
   // Create input elements for each field
   blockChildDivs.forEach((div) => {
@@ -20,30 +25,59 @@ export default function decorate(block) {
       div.classList.add('contact-form__field');
     }
 
+    // Check for field name and add a fitting input "type" attribute and assign variables
+    let firstName;
+    let lastName;
+    let email;
+    let birthDate;
+    let termsOfService;
     // Add inputs with fitting "type" attributes to every field except for the title
     if (!fieldName.includes('title')) {
       // Create input element
       const input = document.createElement('input');
 
-      // Check for field name and add a fitting input "type" attribute
       if (fieldName.includes('name')) {
         input.setAttribute('type', 'text');
       }
+      if (fieldName.includes('first name')) {
+        firstName = input.textContent;
+      }
+      if (fieldName.includes('last name')) {
+        lastName = input.textContent;
+      }
       if (fieldName.includes('email')) {
         input.setAttribute('type', 'email');
-      }
-      if (fieldName.includes('terms of service')) {
-        input.setAttribute('type', 'checkbox');
+        email = input.textContent;
       }
       if (fieldName.includes('date')) {
         input.setAttribute('type', 'date');
+        birthDate = input.textContent;
+      }
+      if (fieldName.includes('terms of service')) {
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('id', 'contact-form__tos-input');
       }
 
-      // Add the input
+      // Add the input to each field
       div.appendChild(input);
     }
 
     // Remove the first column element
     div.removeChild(div.firstElementChild);
+
+    // Specify submitContactInfo arguments
+    const submitInfo = (event) => {
+      // To prevent the button from scrolling up when clicked
+      if (event) {
+        event.preventDefault();
+      }
+      submitContactInfo(firstName, lastName, email, birthDate, termsOfService);
+    };
+
+    // Add event listener
+    if (!submitBtnDiv.getAttribute('has-event-listener')) {
+      submitBtnDiv.addEventListener('click', submitInfo);
+      submitBtnDiv.setAttribute('has-event-listener', 'true');
+    }
   });
 }
---
+++ blocks/contact-form/submit-contact-info.js
@@ -0,0 +1,15 @@
+// export default function submitContactInfo(
+//   firstName,
+//   lastName,
+//   email,
+//   birthDate,
+//   termsOfService
+// ) {
+//   const formData = {
+//     'first-name': firstName,
+//     'last-name': lastName,
+//     email: email,
+//     'date-of-birth': birthDate,
+//     'agree-to-tos': termsOfService,
+//   };
+// }
